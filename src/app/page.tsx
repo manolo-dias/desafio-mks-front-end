c41eb048ae7176d0c2ae08d6afd7dce043d7a51a
Dias <manolo.dias.junior@gmail.com>
2024-06-06 03:19:32 -0300

    feat: add market page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,106 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Product from "@/@components/product";
+import styles from "./styles.module.scss"
+import Container from "@/@components/container";
+const dataBase = {
+  "products": [
+    {
+      "id": 3,
+      "name": "Macbook Air",
+      "brand": "Apple",
+      "description": "Processador intel Core i5 de dois núcleos e 1,8 GHz (Turbo Boost de até 2,9 GHz) com cache L3 compartilhado de 3 MB.",
+      "photo": "https://mks-sistemas.nyc3.digitaloceanspaces.com/products/macbookair.webp",
+      "price": "8200.00",
+      "createdAt": "2023-10-30T16:25:01.093Z",
+      "updatedAt": "2023-10-30T16:25:01.093Z"
+    },
+    {
+      "id": 4,
+      "name": "iPhone 12 64 GB",
+      "brand": "Apple",
+      "description": "Grave vídeos 4K, faça belos retratos e capture paisagens inteiras com o novo sistema de câmera dupla.",
+      "photo": "https://mks-sistemas.nyc3.digitaloceanspaces.com/products/iphone12x64.webp",
+      "price": "6500.00",
+      "createdAt": "2023-10-30T16:25:01.093Z",
+      "updatedAt": "2023-10-30T16:25:01.093Z"
+    },
+    {
+      "id": 1,
+      "name": "Iphone 11 128 GB",
+      "brand": "Apple",
+      "description": "Grave vídeos 4K, faça belos retratos e capture paisagens inteiras com o novo sistema de câmera dupla.",
+      "photo": "https://mks-sistemas.nyc3.digitaloceanspaces.com/products/iphone11x128.webp",
+      "price": "5000.00",
+      "createdAt": "2023-10-30T16:25:01.093Z",
+      "updatedAt": "2023-10-30T16:25:01.093Z"
+    },
+    {
+      "id": 6,
+      "name": "iPad",
+      "brand": "Apple",
+      "description": "iPad é uma linha de tablets projetada, desenvolvida e comercializada pela Apple, que funciona com o sistema operacional móvel iOS e iPadOS.",
+      "photo": "https://mks-sistemas.nyc3.digitaloceanspaces.com/products/ipadair.webp",
+      "price": "4200.00",
+      "createdAt": "2023-10-30T16:25:01.093Z",
+      "updatedAt": "2023-10-30T16:25:01.093Z"
+    },
+    {
+      "id": 8,
+      "name": "Headset Cloud Stinger",
+      "brand": "HyperX",
+      "description": "O HyperX Cloud Stinger™ é o headset ideal para jogadores que procuram leveza e conforto, qualidade de som superior e maior praticidade.",
+      "photo": "https://mks-sistemas.nyc3.digitaloceanspaces.com/products/hyperxcloudstinger.webp",
+      "price": "600.00",
+      "createdAt": "2023-10-30T16:25:01.093Z",
+      "updatedAt": "2023-10-30T16:25:01.093Z"
+    },
+    {
+      "id": 7,
+      "name": "Headset Cloud Revolver",
+      "brand": "HyperX",
+      "description": "A linha HyperX Cloud Revolver foi projetada para atender as exigências dos gamers de PC ou de console.",
+      "photo": "https://mks-sistemas.nyc3.digitaloceanspaces.com/products/hyperx-cloudrevolver.webp",
+      "price": "1000.00",
+      "createdAt": "2023-10-30T16:25:01.093Z",
+      "updatedAt": "2023-10-30T16:25:01.093Z"
+    },
+    {
+      "id": 5,
+      "name": "Apple Watch Series 7",
+      "brand": "Apple",
+      "description": "O Apple Watch faz coisas que outros aparelhos não conseguem porque ele fica no seu pulso.",
+      "photo": "https://mks-sistemas.nyc3.digitaloceanspaces.com/products/applewatch-series7.webp",
+      "price": "3200.00",
+      "createdAt": "2023-10-30T16:25:01.093Z",
+      "updatedAt": "2023-10-30T16:25:01.093Z"
+    },
+    {
+      "id": 2,
+      "name": "AirPods",
+      "brand": "Apple",
+      "description": "Criados pela Apple Ligam e se conectam automaticamente Configuração com apenas um toque para todos seus aparelhos Apple.",
+      "photo": "https://mks-sistemas.nyc3.digitaloceanspaces.com/products/airpods.webp",
+      "price": "1200.00",
+      "createdAt": "2023-10-30T16:25:01.093Z",
+      "updatedAt": "2023-10-30T16:25:01.093Z"
+    }
+  ],
+  "count": 8
+}
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Container>
+    <div className={styles.productGrid}>
+      {dataBase.products.map(product => {
+        return (
+          <Product
+            {...product}
+            key={product.id}
+          />
+        )
+      })}
+    </div>
+    </Container>
   );
+  
 }
