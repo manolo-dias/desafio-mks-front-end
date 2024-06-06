3044a1146bd79bca372fb7f72535398ce62ae4e0
Dias <manolo.dias.junior@gmail.com>
2024-06-06 03:16:48 -0300

    feat: add navbar component

---
+++ src/@components/navbar/index.tsx
@@ -0,0 +1,13 @@
+import ButtonCart from "../button-cart"
+import styles from "./styles.module.scss"
+export default function Navbar(){
+    return(
+        <nav className={styles.navbar}>
+            <div>
+                <h1>MKS</h1>
+                <h2>Sistemas</h2>
+            </div>
+            <ButtonCart/>
+        </nav>
+    )
+}
