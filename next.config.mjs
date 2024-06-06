e8c0d66bfffcafc2634dfc38ab70ca2e8aae6f4f
Dias <manolo.dias.junior@gmail.com>
2024-06-06 03:12:48 -0300

    build: basic setup sass install

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    sassOptions: {
+        includePaths: ["./src"],
+        prependData: `@import "./src/styles/settings.module.scss";`,
+    }
+};
 
 export default nextConfig;
