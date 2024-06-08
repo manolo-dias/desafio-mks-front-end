abb6ee6e560d173536bf3a38a531cb317ad05d08
Dias <manolo.dias.junior@gmail.com>
2024-06-07 21:08:51 -0300

    fix: footer style

--- src/@components/footer/index.tsx
+++ src/@components/footer/index.tsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.scss"
 export default function Footer() {
     return (
         <footer className={styles.footer} >
-            <h1>MKS sistemas © Todos os direitos reservados</h1>
+            <h1 className={styles.footer}>MKS sistemas © Todos os direitos reservados</h1>
         </footer>
     )
 }
